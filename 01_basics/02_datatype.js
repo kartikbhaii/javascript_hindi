5b30fc11827b892d5d60d76058a9d39f7bf12b61
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2025-07-13 13:01:40 +0530

    Update 02_datatype.js

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+
+let name = "Kartikey"
+let age = 22
+let isLoggedIn = false;
+
+// int : number from 2 to the power 53
+// bigint
+// string
+// boolean
+// null: standalone value
+// symbol: defined an individual content on website, for uniqueness purpose. used in figma tools.
+
+
+
+/* NOTE
+
+undefined: means variable are declared
+but you havent initalized it yet.
+
+null: we can assign null to any variable, 
+it means no value is present at variable.
+
+*/
+
+let collegeName;
+console.log(collegeName) // undefined
+
+
+let collegeId = "null"
+console.log(collegeId)
