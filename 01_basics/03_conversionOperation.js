6aacb57cfead133826623fadc8a08f2575a18c26
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2025-07-14 01:33:21 +0530

    Add files via upload

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "kartikey"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber);
+
+// NaN unserstand for "Not A Number".
+
+
+// NOTE:
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+
+let isLoggedIn = 1
+let inBoolean = Boolean(isLoggedIn)
+
+// console.log(inBoolean)
+
+// 1 => true; 0 => false;
+// "" : empty string => false
+// "kartikey": true
+
+let someNo = 33
+let stringNumber = String(someNo)
+console.log(stringNumber)
+console.log(typeof stringNumber);
