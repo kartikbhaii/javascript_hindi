a138b8be329729cc64599c334d9a2214d223d296
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2025-07-14 14:24:43 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,3 +31,46 @@ let someNo = 33
 let stringNumber = String(someNo)
 console.log(stringNumber)
 console.log(typeof stringNumber);
+
+
+// *********************************************** OPERATIONS ********************************************* //
+
+
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+// console.log(2+3)
+// console.log(2*2)
+// console.log(2**5)
+// console.log(2/3)
+// console.log(2%5)
+
+
+let str1 = "Hello"
+let str2 = "Kartikey"
+
+let str3 = str1 + " " + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log( 3 + 4 * 5 % 3);
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+
+let num1 = num2 = num3 = 2+2
+
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
