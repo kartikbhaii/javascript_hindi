7971f13c7dd4201267033e869fffd085418903ce
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2025-07-15 23:48:45 +0530

    Update datatype_summary.js

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -1,5 +1,7 @@
 
-// PRIMITIVE
+// PRIMITIVE AND NON PRIMITIVE
+
+// PRIMITIVE:
 
 // 7 TYPES: String, Number, Boolean, null (means empty, not zero but empty), undefined ( declared but not initialized)
 //          Symbol (making unique component), BigInt
@@ -20,7 +22,7 @@ const anotherId = Symbol('123')
 
 const bigNumber = 51654985451564686465468n
 
-// console.log(bigNumber)
+console.log(bigNumber)
 
 
 
@@ -37,7 +39,7 @@ let myObj = {
     city: "ballia"
 }
 
-// console.log(myObj.name);
+console.log(myObj.name);
 
 
 const myFunction = function(){
@@ -60,6 +62,35 @@ console.log(typeof myFunction); // type of funciton => funciton
 
 
 
+/***************************************************************************/
 
 
+// Stack (Primitive) , Heap (Non-Primitive)
+// stack ke andar jo bhi chize jayengi jaise boolean, integer ... ye sab ko kisi nye variable me store kiya jaye
+// to unke copy nye variable me store hote hai, mtlb isme copies banti hain
+// heap ke andar jo store hota hai jaise objects... usme copy value nhi milti, usme direct value ka reference milta hai,
+// to agar nye variable me heap ka data change kiya jaye to original jagah se bhi change hoga, aur stack me aisa nhi hota
+
+
+let myYoutubeName = "Kartikey Singh"
+let anotherName = myYoutubeName
+console.log(anotherName)
+console.log(myYoutubeName)
+
+anotherName = "tehri aur code"
+console.log(anotherName)
+console.log(myYoutubeName)
+
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "kartikey@microsoft.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
 
