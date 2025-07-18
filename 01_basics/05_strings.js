72aeee1574c19e80b94dbd2de403efd6f20cf97e
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2025-07-19 01:46:42 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,6 +1,8 @@
 const name = "kartikey singh";
 const repoCount = 50;
 // console.log(name + repoCount + " Value")
+
+//NOTE: 
 // USE NEW FORMAT TO USE STRING USING BACKSTRICK `  `
 
 console.log(`Hello world ${name} and my repo count is ${repoCount}`);
@@ -13,5 +15,23 @@ const gameName = new String('kartikey')
 
 // console.log(gameName.length)
 // console.log(gameName.toUpperCase())
-console.log(gameName.charAt(3))
-console.log(gameName.indexOf('t'))
+// console.log(gameName.charAt(3))
+// console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)
+
+const newStringOne = " singh "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://kartikey.com/kartikey%20singh"
+console.log(url)
+
+const urlNew = url.replace('%20', '-')
+console.log(urlNew)
+
+console.log(url.includes('kartikey')) //------------- true!!! ---------------//
