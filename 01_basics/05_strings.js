5f3526f8222926f742018a18bcbfe8637001e3c7
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2025-07-19 01:26:17 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,17 @@
-const name = "hitesh";
+const name = "kartikey singh";
 const repoCount = 50;
-
 // console.log(name + repoCount + " Value")
+// USE NEW FORMAT TO USE STRING USING BACKSTRICK `  `
+
+console.log(`Hello world ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('kartikey')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf('t'))
