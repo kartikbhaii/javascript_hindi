fe2af69ebed34ab2c51d768dbe81fa6bf71f1dd6
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2025-07-25 18:36:45 +0530

    Add files via upload

---
+++ 02_basics/01_array.js
@@ -0,0 +1,21 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeroes = ["shaktimaan", "haatim"]
+
+const arr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1])
+
+//********** ARRAY METHODS *********/
+
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr)
+// myArr.pop()
+console.log(myArr)
+
+// myArr.unshift(9)
+// console.log(myArr)
+
+myArr.shift()
+console.log(myArr)
