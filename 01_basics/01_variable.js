12b9c31d14c0a638e521f2bf940b7b89a23ab373
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2025-07-26 10:38:00 +0530

    Update 01_variable.js

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -4,9 +4,7 @@ var accountPassword = "Jaipur"
 accountState = "In Process"
 let account;
 
-
-// accountId = 11111;
-
+accountId = 11111;
 accountEmail = "hello@hello"
 accountPassword = 12345678
 accountState = "completed"
@@ -20,6 +18,5 @@ and functional scope
 
 
 console.log(accountId);
-
 
 console.table([accountId, accountEmail, accountPassword, accountState, account])
