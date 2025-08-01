ca989d6fe1be60f230dedd35b5ee62803b7a26d6
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2025-08-01 23:10:06 +0530

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,16 +1,16 @@
-// console.log(2>1)
-// console.log(2<1)
-// console.log(2==1)
+console.log(2>1)
+console.log(2<1)
+console.log(2==1)
 
-// console.log("2" > 1)
+console.log("2" > 1)
 
-// console.log("2"===1)
+console.log("2"===1)
 
-// console.log(2+4-"3"+6-2)
+console.log(2+4-"3"+6-2)
 
-// console.log(null>0)
-// console.log(null<0)
-// console.log(null>=0)
+console.log(null>0)
+console.log(null<0)
+console.log(null>=0)
 
 //NOTE:
 // Comparison check(>, <... etc) works different than the equality check ( == )
@@ -18,10 +18,10 @@
 // gets converted to 0 and checking (0 > 0). so it is giving false as output.
 // And equality opr works differently thats why (null>=0) gives true while (null>0) gives false
 
-// console.log(undefined>0)
-// console.log(undefined<0)
-// console.log(undefined>=0)
-// console.log(undefined<=0)
+console.log(undefined>0)
+console.log(undefined<0)
+console.log(undefined>=0)
+console.log(undefined<=0)
 
 // NOTE:
 // comparing undefined to 0 gives false in every case. you can refer to documentation.
