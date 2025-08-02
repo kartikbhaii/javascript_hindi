f8bc93c3b060b8f9f18432bdcbde85f6095fdf7b
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2025-08-02 22:18:38 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -9,14 +9,14 @@ console.log(`Hello world ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('kartikey')
 
-// console.log(gameName[0])
-// console.log(gameName.__proto__)
+console.log(gameName[0])
+console.log(gameName.__proto__)
 
 
-// console.log(gameName.length)
-// console.log(gameName.toUpperCase())
-// console.log(gameName.charAt(3))
-// console.log(gameName.indexOf('t'))
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf('t'))
 
 const newString = gameName.substring(0,4)
 console.log(newString)
@@ -35,5 +35,6 @@ const urlNew = url.replace('%20', '-')
 console.log(urlNew)
 
 console.log(url.includes('kartikey')) //------------- true!!! ---------------//
+
 
 
